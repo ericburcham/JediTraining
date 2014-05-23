57d98d63415fdf46247ad23ddf81d2b9af4ac0ef
Devlin Liles <devlin@devlinliles.com>
2014-05-23 15:06:27 -0500

    Pushing karma setup

--- test/InvocationPatterns/invocationTests.js
+++ test/InvocationPatterns/invocationTests.js
@@ -1,4 +1,25 @@
 describe("When using the constructor invocation pattern", function(){
+  var ThisConstructor = function(){
+    this.foo = 'bar';
+  }
+  
+  var ThisConstructorThrowsThings = function(){
+    if(!(this instanceof arguments.callee)){
+      throw new Error("Constructor called without the new keyword");
+    }
+  }
+
   it("should return a new object", function(){
+      expect(new ThisConstructor()).not.toBe(null);
+  });
+  
+  it("Should polute the global namespace if called without the new keyword", function(){
+    ThisConstructor();
+    
+    expect(foo).toBe('bar');
+  })
+  
+  it ("Should throw an exception when called without the new keyword", function(){
+    expect( function() { ThisConstructorThrowsThings(); }).toThrow(new Error("Constructor called without the new keyword"));
   });
 })
